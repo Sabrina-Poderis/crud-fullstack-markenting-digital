a03e1f379d09eabf47799ae4d2c76ff38f711f04
Sabrina Poderis <54513989+Sabrina-Poderis@users.noreply.github.com>
2025-03-27 02:30:20 -0300

    Revert "Feat/2/clients module"

--- backend/src/utils/formatJoiValidation.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-const formatJoiValidation = (error: Joi.ValidationError | undefined) => {
-  return error
-    ? { hasError: true, message: error.message }
-    : { hasError: false };
-}
-
-export default formatJoiValidation

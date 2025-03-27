81a29b16370691e2310ff22257837a7c71b438b1
Sabrina-Poderis <sabrina.poderis@gmail.com>
2025-03-26 21:05:21 -0300

    feat: include database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,25 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import db from "./config/database"; // Se estiver usando Knex
+import env from "./config/env";
+dotenv.config();
 
+const PORT = env.PORT
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.get("/healthCheck", (_req, res) => {
-  res.json({ status: "ok" });
+app.use(cors());
+app.use(express.json());
+
+app.get("/healthCheck", async (req, res) => {
+  try {
+    res.json({ status: "ok", db: "connected" });
+  } catch (error) {
+    res.status(500).json({ status: "error", db: "disconnected" });
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, async () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+
 });

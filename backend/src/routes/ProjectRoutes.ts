b4d9140a5acbeb0393210158953803dbcfee5b49
Sabrina-Poderis <sabrina.poderis@gmail.com>
2025-03-28 17:08:30 -0300

    fix: bind routes

--- backend/src/routes/ProjectRoutes.ts
+++ backend/src/routes/ProjectRoutes.ts
@@ -5,10 +5,10 @@ import authMiddleware from '../middleware/authMiddleware';
 const ProjectRoutes = express.Router();
 const projectController = new ProjectController();
 
-ProjectRoutes.get('/', authMiddleware, projectController.findAll);
-ProjectRoutes.get('/:id', authMiddleware, projectController.findOne);
-ProjectRoutes.post('/', authMiddleware, projectController.create);
-ProjectRoutes.put('/:id', authMiddleware, projectController.update);
-ProjectRoutes.delete('/:id', authMiddleware, projectController.delete);
+ProjectRoutes.get('/', authMiddleware, projectController.findAll.bind(ProjectController));
+ProjectRoutes.get('/:id', authMiddleware, projectController.findOne.bind(ProjectController));
+ProjectRoutes.post('/', authMiddleware, projectController.create.bind(ProjectController));
+ProjectRoutes.put('/:id', authMiddleware, projectController.update.bind(ProjectController));
+ProjectRoutes.delete('/:id', authMiddleware, projectController.delete.bind(ProjectController));
 
 export default ProjectRoutes;

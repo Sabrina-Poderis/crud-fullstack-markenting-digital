33ffb23950a0051db91de79ca81e3ef6aa41d682
Sabrina-Poderis <sabrina.poderis@gmail.com>
2025-03-28 01:00:06 -0300

    feat: create projects pages

---
+++ frontend/src/pages/admin/projects/ProjectsList.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <h1>Lista de Projetos</h1>
+
+    <p v-if="projects.length === 0">Sem projetos cadastrados</p>
+    <template v-else>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Data</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="project in projects" :key="project.id">
+            <td>{{ project.name }}</td>
+            <td>{{ project.startDate }} - {{ project.endDate }}</td>
+            <td>
+              <button @click="editProject(project.id)">Editar</button>
+              <button @click="deleteProject(project.id)">Excluir</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </template>
+
+    <div class="buttons_container">
+      <button @click="goToCreateClient">Novo Cliente</button>
+      <button type="button" @click="goBack">Voltar</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, type Ref } from "vue";
+import { useRouter } from "vue-router";
+import { ProjectsService } from "../../../services/ProjectsService";
+import type { Projects } from "../../../ts/types/Project";
+
+const router = useRouter();
+const projects: Ref<Projects[]> = ref([]);
+
+const fetchProjects = async () => {
+  try {
+    projects.value = await ProjectsService.get();
+  } catch (error) {
+    console.error("Erro ao buscar clientes", error);
+  }
+};
+
+const goBack = () => {
+  router.back();
+};
+
+const goToCreateClient = () => {
+  router.push("/projects/new");
+};
+
+const editProject = (id: number) => {
+  router.push(`/projects/${id}/edit`);
+};
+
+const deleteProject = async (id: number) => {
+  try {
+    await ProjectsService.delete(id);
+    await fetchProjects();
+  } catch (error) {
+    console.error("Erro ao excluir cliente", error);
+  }
+};
+
+onMounted(fetchProjects);
+</script>

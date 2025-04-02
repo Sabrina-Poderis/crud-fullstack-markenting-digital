d591b2343fea0c489aec93d752e21efa1ad2256e
Sabrina-Poderis <sabrina.poderis@gmail.com>
2025-04-02 18:08:25 -0300

    fix: docker build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+  return {
+    plugins: [vue()],
+    build: {
+      outDir: 'dist',
+      sourcemap: true,
+    },
+  }
 })
